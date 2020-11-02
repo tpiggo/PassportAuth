e84bfdb5c74e4f5732a25cf13a78a007baab1e52
Timothy Piggott <piggott291@gmail.com>
2020-11-01 23:14:54 -0500

    Final build, clean and comment

--- PassportAuth/routes/users.js
+++ PassportAuth/routes/users.js
@@ -83,4 +83,10 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.flash('success_msg', 'You are now logged out!')
+    res.redirect('/users/login');
+})
+
 module.exports = router;
